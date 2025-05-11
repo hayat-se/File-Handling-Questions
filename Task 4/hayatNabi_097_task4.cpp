3bff0ee0bbe8404f1ba82f288794c1802ef48e09
hayat-se <hayatlucky064@gmail.com>
2025-05-11 14:06:25 +0500

    Adding Task 4

---
+++ Task 4/hayatNabi_097_task4.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+    ofstream data("data.text" , ios::app);
+    string name ,date_Of_Birth, place_Of_Birth;
+    int age;
+    float cgpa;
+    int counter = 0;
+    if(data.is_open())
+    {
+        char ch;
+        cout<<"** Files Opened Successfuly **\n\n";
+        do
+        {
+            cout<<"Enter Name : ";
+            getline(cin,name);
+            cout<<"Enter Age : ";
+            cin>>age;
+            cout<<"Enter CGPA : ";
+            cin>>cgpa;
+            cin.ignore();
+            cout<<"Enter Date of Birth : ";
+            getline(cin , date_Of_Birth);
+            cout<<"Enter Place of Birth : ";
+            getline(cin,place_Of_Birth);
+
+            data << "Student Record Number "<<++counter << " : \n\n" << "Name : "<<name <<"\nAge : "<<age <<"\nCGPA : " <<cgpa <<"\nDOB : "<<date_Of_Birth <<"\nPlace Of Birth : "<<place_Of_Birth <<"\n\n";
+            cout<<"Data Store Successfuly ....\n";
+            cout <<"\nDo You want to Add Another Student Data (Y/N): ";
+            cin>>ch;
+            cin.ignore();
+        }while(ch == 'Y' || 'y');
+    }
+    else
+    {
+        cout<<"Error to Open File \n";
+    }
+    data.close();
+}
